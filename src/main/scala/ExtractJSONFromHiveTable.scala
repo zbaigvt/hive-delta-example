26c15b12a680715682f872fc01aef6d11ad39380
Zeeshan Baig <zbaig@verticaltrail.com>
2017-12-13 14:47:59 -0600

    JSON to hive working for 3 columns

--- src/main/scala/ExtractJSONFromHiveTable.scala
+++ src/main/scala/ExtractJSONFromHiveTable.scala
@@ -1,27 +1,47 @@
+import org.apache.spark.sql.{Row, SaveMode}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.functions.explode
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
 object ExtractJSONFromHiveTable {
 
   var appName = "ExtractJSONFromHiveTable"
-  var master = "local"
+ // var master = "local"
+  // The schema is encoded in a string
+  val schemaString = "name type subscription_id"
 
   def main(args: Array[String]): Unit = {
 
-    val conf = new SparkConf().setAppName(appName).setMaster(master)
+    val conf = new SparkConf().setAppName(appName)//.setMaster(master)
     val sc = new SparkContext(conf)
     val hc = new org.apache.spark.sql.hive.HiveContext(sc)
 
-    val notificationsData = hc.sql("select document_id, lower(document) as document from myqdatawarehouse.myquserdata_entry where instr(document_id, 'subscription_') != 0 limit 10")
+    // Read data from Hive table
+    val notificationsData = hc.sql("select document from myqdatawarehouse.myquserdata_entry where instr(document_id, 'subscription_') != 0 limit 10")
 
-    val jsonData = hc.read.json(notificationsData.map{r=> r.getString(1)})
+    //Convert it to JSON
+    val jsonData = hc.read.json(notificationsData.map(r=> r.getString(0)))
 
-    val count = jsonData.count()
+    //Select attributes
+    val record = jsonData.select(jsonData("name"),jsonData("type"),jsonData("subscription_id"))
 
-    val record = jsonData.select(jsonData("type"), explode(jsonData("events.values")).alias("events"))
+    // Convert records of the RDD to Rows.
+    val rowRDD = record.map(r => Row(r.getString(0),r.getString(1),r.getString(2))) // Row(r(0), r(1)))
 
-    record.show(5)
-    println("Total records " + count)
+    // Generate the schema
+    val schema = StructType(Array(StructField("name",StringType,true),
+                                 StructField("type",StringType,true),
+                                 StructField("subscription_id",StringType,true))
+                          )
+
+    // Apply the schema to the RDD.
+    val notificationDataFrame = hc.createDataFrame(rowRDD, schema)
+
+    // Register the DataFrames as a table.
+
+    notificationDataFrame.write.mode(SaveMode.Overwrite).saveAsTable("myqdatawarehouse.notifications")
+
+  //  println("Total records " + count)
   }
 
 }
