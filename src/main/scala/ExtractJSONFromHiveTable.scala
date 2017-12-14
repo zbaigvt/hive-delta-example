6c625caae305fb98bcbcb1290f625f92ce5cf9aa
Zeeshan Baig <zbaig@verticaltrail.com>
2017-12-14 10:53:53 -0600

    working on exploding arrays

--- src/main/scala/ExtractJSONFromHiveTable.scala
+++ src/main/scala/ExtractJSONFromHiveTable.scala
@@ -67,33 +67,94 @@ object ExtractJSONFromHiveTable {
     val sc = new SparkContext(conf)
     val hc = new org.apache.spark.sql.hive.HiveContext(sc)
 
-    // Read data from Hive table
-   // val notificationsData = hc.sql("select document_id, lower(document) as document from myqdatawarehouse.myquserdata_entry where instr(document_id, 'subscription_') != 0 limit 10")
+    // Read data from Hive table.adding document_id to JSON structure to easily retrieve later in jsonData RDD
     val notificationsData = hc.sql("select concat('{\"document_id\":','\"',document_id,'\", ',substring(document,2)) as document from myqdatawarehouse.myquserdata_entry where instr(document_id, 'subscription_') != 0 limit 10")
+
     //Convert it to JSON
     val jsonData = hc.read.json(notificationsData.map{r=> r.getString(0)})
 
-    //Select attributes explode(jsonData("device_serial_numbers.values")).alias("device_serial_numbers"))
-    //val record = jsonData.select(jsonData("name"),jsonData("type"),jsonData("subscription_id"))
-   // notificationsData("document_id")
-
-    val record =  jsonData.select( jsonData("document_id"),
-                                   jsonData("name"),
+    val masterRecord =  jsonData.select(
+                                   jsonData("document_id").alias("master_doc_id"),
                                    jsonData("type"),
                                    jsonData("subscription_id"),
-                                   explode(jsonData("device_serial_numbers.values")).alias("device_serial_numbers"))
-    // need doc id to join val events = jsonData.select(jsonData)
+                                   jsonData("access_schedule_id"),
+                                   jsonData("account_users"),
+                                   jsonData("access_groups"),
+                                   jsonData("nest_zone_ids"),
+                                   jsonData("name"),
+                                   jsonData("enabled"),
+                                   jsonData("notification_delay_minutes"),
+                                   jsonData("created_on_time"),
+                                   jsonData("updated_on_time"),
+                                   explode(jsonData("device_serial_numbers.values")).alias("device_serial_numbers")
+                                   )
+
+    val eventsRecord = jsonData.select(
+                                    jsonData("document_id").alias("event_doc_id"),
+                                    explode(jsonData("events.values")).alias("events"))
+
+    val emailNotificationRecord =  jsonData.select(
+                                         jsonData("document_id").alias("email_doc_id"),
+                                         explode(jsonData("email_notification_list.values")).alias("email_notification_list"))
+
+    val timeBlocksRecord = jsonData.select(
+                                       jsonData("document_id").alias("time_doc_id"),
+                                       jsonData("time_blocks.values.from_time").getItem(0),
+                                       jsonData("time_blocks.values.to_time").getItem(0),
+                                        explode(jsonData("time_blocks.values.day")).alias("days"))
+
+
+    val notificationTypesRecord =  jsonData.select(
+                                       jsonData("document_id").alias("notify_doc_id"),
+                                       explode(jsonData("notification_types.values")).alias("notification_types"))
+
+    //TODO join RDDs to produce one result + update schema with new changes
+    val joinEvents = masterRecord.join(eventsRecord,masterRecord("master_doc_id")===eventsRecord("event_doc_id"), "left_outer")
+    val joinEmail = joinEvents.join(emailNotificationRecord,joinEvents("master_doc_id")===emailNotificationRecord("email_doc_id"),"left_outer")
+    val joinNotifyTypes = joinEmail.join(notificationTypesRecord,joinEmail("master_doc_id")===notificationTypesRecord("notify_doc_id"),"left_outer")
+
 
 
     // Convert records of the RDD to Rows.
-    val rowRDD = record.map(r => Row(r.getString(0),r.getString(1),r.getString(2),r.getString(3), r.getString(4))) // Row(r(0), r(1)))
+    val rowRDD = joinNotifyTypes.map(r => Row(r.getString(0),
+                                            r.getString(1),
+                                            r.getString(2),
+                                            r.getString(3),
+                                            r.getString(4),
+                                            r.getString(6)))
 
     // Generate the schema
     val schema = StructType(Array(StructField("document_id",StringType,true),
-                                 StructField("name",StringType,true),
-                                 StructField("type",StringType,true),
-                                 StructField("subscription_id",StringType,true),
-                                 StructField("device_serial_number",StringType,true))
+                                  StructField("name",StringType,true),
+                                  StructField("type",StringType,true),
+                                  StructField("subscription_id",StringType,true),
+                                  StructField("access_schedule_id",StringType,true),
+                                  StructField("account_users",StringType,true),
+                                  StructField("access_groups",StringType,true),
+                                  StructField("email_notification_list",StringType,true),
+                                  StructField("nest_zone_ids",StringType,true),
+                                  StructField("enabled",StringType,true),
+                                  StructField("notification_delay_minutes",StringType,true),
+                                  StructField("event_open",StringType,true),
+                                  StructField("event_close",StringType,true),
+                                  StructField("events_on",StringType,true),
+                                  StructField("event_off",StringType,true),
+                                  StructField("access_events",StringType,true),
+                                  StructField("days_of_week",StringType,true),
+                                  StructField("from_time",StringType,true),
+                                  StructField("to_time",StringType,true),
+                                  StructField("account_user_id",StringType,true),
+                                  StructField("device_serial_number",StringType,true),
+                                  StructField("push_notification",StringType,true),
+                                  StructField("email_notification",StringType,true),
+                                  StructField("created_on_time",StringType,true),
+                                  StructField("updated_on_time",StringType,true),
+                                  StructField("is_default",StringType,true),
+                                  StructField("entry_code_uses_exceeded",StringType,true),
+                                  StructField("entry_code_uses_exceeded_id",StringType,true),
+                                  StructField("entry_code_uses_exceeded_amount",StringType,true),
+                                  StructField("entry_code_uses_exceeded_time_period",StringType,true)
+                                )
                           )
 
     // Apply the schema to the RDD.
